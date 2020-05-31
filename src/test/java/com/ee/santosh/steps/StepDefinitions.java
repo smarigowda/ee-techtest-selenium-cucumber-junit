5ee622772131c3a44667ec83d068d75496abf39f
Santosh Marigowda <santosharakere@gmail.com>
2020-05-31 19:29:43 +0100

    removed all the code for reading data from json file

--- src/test/java/com/ee/santosh/steps/StepDefinitions.java
+++ src/test/java/com/ee/santosh/steps/StepDefinitions.java
@@ -1,10 +1,6 @@
 package com.ee.santosh.steps;
 
 import com.ee.santosh.pageobjects.HomePage;
-import com.ee.santosh.pojo.classes.Data;
-import com.ee.santosh.pojo.classes.DataItem;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
@@ -17,26 +13,20 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 @RunWith(Cucumber.class)
 public class StepDefinitions {
 
+    // TO DO: Move the url into properties file.
     private final String url = "http://hotel-test.equalexperts.io/";
-    private final String jsonDataFile = "src/test/resources/data/data.json";
+
+    // TO DO:
+    // Move the initialization of driver into another class
+    // And update this class to inherit from it.
     private WebDriver driver;
     private HomePage homePage;
-    private Data data;
 
     @Given("^User opens the application$")
-    public void user_opens_the_application() throws IOException, InterruptedException {
-        data = new Data();
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String fileData = new String(Files.readAllBytes(Paths.get(jsonDataFile)));
-        data = gson.fromJson(fileData, Data.class);
-
+    public void user_opens_the_application() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().deleteAllCookies();
